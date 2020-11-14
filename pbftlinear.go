c11ffc29f9d8f073120bb7c2aff0d1ddf291ec75
zxh <178559145@qq.com>
2020-11-14 18:57:14 +0800

    建立grpc的连接了

--- pbftlinear.go
+++ pbftlinear.go
@@ -3,13 +3,12 @@ package pbftlinear
 import (
 	"context"
 	"crypto/ecdsa"
-	"crypto/rand"
 	"crypto/sha512"
 	"encoding/base64"
 	"encoding/binary"
-	"errors"
 	"fmt"
-	"github.com/joe-zxh/pbftlinear/util"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"log"
 	"math/big"
 	"net"
@@ -19,7 +18,6 @@ import (
 
 	"github.com/joe-zxh/pbftlinear/config"
 	"github.com/joe-zxh/pbftlinear/consensus"
-	"github.com/joe-zxh/pbftlinear/data"
 	"github.com/joe-zxh/pbftlinear/internal/logging"
 	"github.com/joe-zxh/pbftlinear/internal/proto"
 	"google.golang.org/grpc"
@@ -36,16 +34,16 @@ func init() {
 // PBFTLinear is a thing
 type PBFTLinear struct {
 	*consensus.PBFTLinearCore
+	proto.UnimplementedPBFTLinearServer
+
 	tls bool
 
-	nodes map[config.ReplicaID]*proto.Node
+	nodes map[config.ReplicaID]*proto.PBFTLinearClient
+	conns map[config.ReplicaID]*grpc.ClientConn
 
-	server  *pbftLinearServer
-	manager *proto.Manager
-	cfg     *proto.Configuration
+	server *pbftLinearServer
 
-	closeOnce sync.Once
-
+	closeOnce      sync.Once
 	connectTimeout time.Duration
 }
 
@@ -53,7 +51,8 @@ type PBFTLinear struct {
 func New(conf *config.ReplicaConfig, tls bool, connectTimeout, qcTimeout time.Duration) *PBFTLinear {
 	pbftlinear := &PBFTLinear{
 		PBFTLinearCore: consensus.New(conf),
-		nodes:          make(map[config.ReplicaID]*proto.Node),
+		nodes:          make(map[config.ReplicaID]*proto.PBFTLinearClient),
+		conns:          make(map[config.ReplicaID]*grpc.ClientConn),
 		connectTimeout: connectTimeout,
 	}
 	return pbftlinear
@@ -62,10 +61,12 @@ func New(conf *config.ReplicaConfig, tls bool, connectTimeout, qcTimeout time.Du
 //Start starts the server and client
 func (pbftlinear *PBFTLinear) Start() error {
 	addr := pbftlinear.Config.Replicas[pbftlinear.Config.ID].Address
+	fmt.Println(`before startServer`)
 	err := pbftlinear.startServer(addr)
 	if err != nil {
 		return fmt.Errorf("Failed to start GRPC Server: %w", err)
 	}
+	fmt.Println(`before startClient`)
 	err = pbftlinear.startClient(pbftlinear.connectTimeout)
 	if err != nil {
 		return fmt.Errorf("Failed to start GRPC Clients: %w", err)
@@ -75,37 +76,7 @@ func (pbftlinear *PBFTLinear) Start() error {
 
 // 作为rpc的client端，调用其他hsserver的rpc。
 func (pbftlinear *PBFTLinear) startClient(connectTimeout time.Duration) error {
-	idMapping := make(map[string]uint32, len(pbftlinear.Config.Replicas)-1)
-	for _, replica := range pbftlinear.Config.Replicas {
-		if replica.ID != pbftlinear.Config.ID {
-			idMapping[replica.Address] = uint32(replica.ID)
-		}
-	}
 
-	// embed own ID to allow other replicas to identify messages from this replica
-	md := metadata.New(map[string]string{
-		"id": fmt.Sprintf("%d", pbftlinear.Config.ID),
-	})
-
-	perNodeMD := func(nid uint32) metadata.MD {
-		var b [4]byte
-		binary.LittleEndian.PutUint32(b[:], nid)
-		hash := sha512.Sum512(b[:])
-		R, S, err := ecdsa.Sign(rand.Reader, pbftlinear.Config.PrivateKey, hash[:])
-		if err != nil {
-			panic(fmt.Errorf("Could not sign proof for replica %d: %w", nid, err))
-		}
-		md := metadata.MD{}
-		md.Append("proof", base64.StdEncoding.EncodeToString(R.Bytes()), base64.StdEncoding.EncodeToString(S.Bytes()))
-		return md
-	}
-
-	mgrOpts := []proto.ManagerOption{
-		proto.WithDialTimeout(connectTimeout),
-		proto.WithNodeMap(idMapping),
-		proto.WithMetadata(md),
-		proto.WithPerNodeMetadata(perNodeMD),
-	}
 	grpcOpts := []grpc.DialOption{
 		grpc.WithBlock(),
 		grpc.WithReturnConnectionError(),
@@ -117,21 +88,19 @@ func (pbftlinear *PBFTLinear) startClient(connectTimeout time.Duration) error {
 		grpcOpts = append(grpcOpts, grpc.WithInsecure())
 	}
 
-	mgrOpts = append(mgrOpts, proto.WithGrpcDialOptions(grpcOpts...))
-
-	mgr, err := proto.NewManager(mgrOpts...)
-	if err != nil {
-		return fmt.Errorf("Failed to connect to replicas: %w", err)
-	}
-	pbftlinear.manager = mgr
-
-	for _, node := range mgr.Nodes() {
-		pbftlinear.nodes[config.ReplicaID(node.ID())] = node
-	}
-
-	pbftlinear.cfg, err = pbftlinear.manager.NewConfiguration(pbftlinear.manager.NodeIDs(), &struct{}{})
-	if err != nil {
-		return fmt.Errorf("Failed to create configuration: %w", err)
+	for rid, replica := range pbftlinear.Config.Replicas {
+		if replica.ID != pbftlinear.Config.ID {
+			conn, err := grpc.Dial(replica.Address, grpcOpts...)
+			if err != nil {
+				log.Fatalf("connect error: %v", err)
+				conn.Close()
+			} else {
+				fmt.Printf("connect sucess to: %d\n", rid)
+				pbftlinear.conns[rid] = conn
+				c := proto.NewPBFTLinearClient(conn)
+				pbftlinear.nodes[rid] = &c
+			}
+		}
 	}
 
 	return nil
@@ -144,20 +113,18 @@ func (pbftlinear *PBFTLinear) startServer(port string) error {
 		return fmt.Errorf("Failed to listen to port %s: %w", port, err)
 	}
 
-	serverOpts := []proto.ServerOption{}
 	grpcServerOpts := []grpc.ServerOption{}
 
 	if pbftlinear.tls {
 		grpcServerOpts = append(grpcServerOpts, grpc.Creds(credentials.NewServerTLSFromCert(pbftlinear.Config.Cert)))
 	}
 
-	serverOpts = append(serverOpts, proto.WithGRPCServerOptions(grpcServerOpts...))
+	pbftlinear.server = newPBFTLinearServer(pbftlinear) // todo: 实现preprepare、prepare、commit
 
-	pbftlinear.server = newPBFTLinearServer(pbftlinear, proto.NewGorumsServer(serverOpts...))
-	s := grpc.NewServer()
-	pbftlinear.server.RegisterPBFTLinearServer(pbftlinear.server)
+	s := grpc.NewServer(grpcServerOpts...)
+	proto.RegisterPBFTLinearServer(s, pbftlinear.server)
 
-	go pbftlinear.server.Serve(lis)
+	go s.Serve(lis)
 	return nil
 }
 
@@ -165,12 +132,20 @@ func (pbftlinear *PBFTLinear) startServer(port string) error {
 func (pbftlinear *PBFTLinear) Close() {
 	pbftlinear.closeOnce.Do(func() {
 		pbftlinear.PBFTLinearCore.Close()
-		pbftlinear.manager.Close()
-		pbftlinear.server.Stop()
+		for _, conn := range pbftlinear.conns { // close clients connections
+			conn.Close()
+		}
 	})
 }
 
-// Propose broadcasts a new proposal(Pre-Prepare) to all replicas
+// 这个server是面向 集群内部的。
+type pbftLinearServer struct {
+	*PBFTLinear
+
+	mut     sync.RWMutex
+	clients map[context.Context]config.ReplicaID
+}
+
 func (pbftlinear *PBFTLinear) Propose(timeout bool) {
 	pp := pbftlinear.CreateProposal(timeout)
 	if pp == nil {
@@ -179,204 +154,22 @@ func (pbftlinear *PBFTLinear) Propose(timeout bool) {
 	logger.Printf("[B/PrePrepare]: view: %d, seq: %d, (%d commands)\n", pp.View, pp.Seq, len(pp.Commands))
 	protobuf := proto.PP2Proto(pp)
 
-	for i, sz := 1, len(pbftlinear.nodes); i <= sz; i++ {
-		go func(id int) {
-			var ppReply *proto.PrePrepareReply
-			var err error
-
-			ppReply, err = pbftlinear.nodes[config.ReplicaID(i)].PrePrepare(context.Background(), protobuf)
-			if err != nil {
-				fmt.Printf("PrePrepare RPC error: %v\n", err)
-			}
-			pbftlinear.handlePPReply(ppReply)
-			// fwefawfawf
-		}(i)
-	}
-
-	// 通过gorums的cfg进行multicast，multicast应该是 不会发送消息给自己的。
-
-	pbftlinear.handlePrePrepare(pp) // leader自己也要处理proposal
-}
-
-func (pbftlinear *PBFTLinear) handlePPReply(ppr *proto.PrePrepareReply) {
-	// 收集签名，准备发起prepare
-}
-
-func (pbftlinear *PBFTLinear) handlePrePrepare(pp *data.PrePrepareArgs) (*proto.PrePrepareReply, error) {
-	return nil, nil
-
-	//pbftlinear.PBFTLinearCore.Mut.Lock()
-	//
-	//if !pbftlinear.Changing && pbftlinear.View == pp.View {
-	//
-	//	ent := pbftlinear.GetEntry(data.EntryID{V: pp.View, N: pp.Seq})
-	//	pbftlinear.PBFTLinearCore.Mut.Unlock()
-	//
-	//	ent.Mut.Lock()
-	//	if ent.Digest == nil {
-	//		ent.PP = pp
-	//		ent.Hash()
-	//		p := &proto.PrepareArgs{
-	//			View:   pp.View,
-	//			Seq:    pp.Seq,
-	//			Digest: ent.Digest.ToSlice(),
-	//		}
-	//		ent.Mut.Unlock()
-	//
-	//		logger.Printf("[B/Prepare]: view: %d, seq: %d\n", pp.View, pp.Seq)
-	//		pbftlinear.cfg.Prepare(p)
-	//		dp := p.Proto2P()
-	//		dp.Sender = pbftlinear.ID
-	//		pbftlinear.handlePrepare(dp)
-	//	} else {
-	//		ent.Mut.Unlock()
-	//		fmt.Println(`接收到多个具有相同seq的preprepare`)
-	//	}
-	//
-	//} else {
-	//	pbftlinear.PBFTLinearCore.Mut.Unlock()
-	//}
-}
-
-func (pbftlinear *pbftLinearServer) PrePrepare(ctx context.Context, protoPP *proto.PrePrepareArgs) (*proto.PrePrepareReply, error) {
-	fmt.Println(`Hi, in PrePrepare GRPC...`)
-	dpp := protoPP.Proto2PP()
-	id, err := pbftlinear.getClientID(ctx)
-	if err != nil {
-		logger.Printf("Failed to get client ID: %v", err)
-		return nil, err
-	}
-	if uint32(id) == pbftlinear.Leader { // 只处理来自leader的preprepare
-		return pbftlinear.handlePrePrepare(dpp)
-	} else {
-		return nil, errors.New(`you are not leader, fuck you`)
-	}
-}
-
-func (pbftlinear *PBFTLinear) handlePrepare(p *data.PrepareArgs) {
-
-	pbftlinear.Mut.Lock()
-
-	if !pbftlinear.Changing && pbftlinear.View == p.View {
-		ent := pbftlinear.GetEntry(data.EntryID{p.View, p.Seq})
-		pbftlinear.Mut.Unlock()
-
-		ent.Mut.Lock()
-
-		ent.P = append(ent.P, p)
-		if ent.PP != nil && !ent.SendCommit && pbftlinear.Prepared(ent) {
-
-			c := &proto.CommitArgs{
-				View:   ent.PP.View,
-				Seq:    ent.PP.Seq,
-				Digest: ent.Digest.ToSlice(),
-			}
-
-			ent.SendCommit = true
-			ent.Mut.Unlock()
-
-			logger.Printf("[B/Commit]: view: %d, seq: %d\n", p.View, p.Seq)
-			pbftlinear.cfg.Commit(c)
-			dc := c.Proto2C()
-			dc.Sender = pbftlinear.ID
-			pbftlinear.handleCommit(dc)
-		} else {
-			ent.Mut.Unlock()
+	for rid, client := range pbftlinear.nodes {
+		if rid != pbftlinear.Config.ID {
+			(*client).PrePrepare(context.TODO(), protobuf)
 		}
-	} else {
-		pbftlinear.Mut.Unlock()
 	}
 }
 
-func (pbftlinear *pbftLinearServer) Prepare(ctx context.Context, protoP *proto.PrepareArgs) {
-	dp := protoP.Proto2P()
-	id, err := pbftlinear.getClientID(ctx)
-	if err != nil {
-		logger.Printf("Failed to get client ID: %v", err)
-		return
-	}
-	dp.Sender = uint32(id)
-	pbftlinear.handlePrepare(dp)
+func (pbftlinear *PBFTLinear) PrePrepare(context.Context, *proto.PrePrepareArgs) (*proto.PrePrepareReply, error) {
+	fmt.Println(`fuckyou`)
+	return nil, status.Errorf(codes.Unimplemented, "method PrePrepare not implemented")
 }
 
-func (pbftlinear *PBFTLinear) handleCommit(c *data.CommitArgs) {
-	pbftlinear.Mut.Lock()
-
-	if !pbftlinear.Changing && pbftlinear.View == c.View {
-		ent := pbftlinear.GetEntry(data.EntryID{c.View, c.Seq})
-		pbftlinear.Mut.Unlock()
-
-		ent.Mut.Lock()
-		ent.C = append(ent.C, c)
-		if !ent.Committed && ent.SendCommit && pbftlinear.Committed(ent) {
-			logger.Printf("Committed entry: view: %d, seq: %d\n", ent.PP.View, ent.PP.Seq)
-			ent.Committed = true
-
-			elem := util.PQElem{
-				Pri: int(ent.PP.Seq),
-				C:   ent.PP.Commands,
-			}
-			ent.Mut.Unlock()
-			pbftlinear.Mut.Lock()
-
-			inserted := pbftlinear.ApplyQueue.Insert(elem)
-			if !inserted {
-				panic("Already insert some request with same sequence")
-			}
-
-			for i, sz := 0, pbftlinear.ApplyQueue.Length(); i < sz; i++ { // commit需要按global seq的顺序
-				m, err := pbftlinear.ApplyQueue.GetMin()
-				if err != nil {
-					break
-				}
-				if int(pbftlinear.Apply+1) == m.Pri {
-					pbftlinear.Apply++
-					cmds, ok := m.C.([]data.Command)
-					if ok {
-						pbftlinear.Exec <- cmds
-					}
-					pbftlinear.ApplyQueue.ExtractMin()
-
-				} else if int(pbftlinear.Apply+1) > m.Pri {
-					panic("This should already done")
-				} else {
-					break
-				}
-			}
-			pbftlinear.Mut.Unlock()
-		} else {
-			ent.Mut.Unlock()
-		}
-	} else {
-		pbftlinear.Mut.Unlock()
-	}
-}
-
-func (pbftlinear *pbftLinearServer) Commit(ctx context.Context, protoC *proto.CommitArgs) {
-	dc := protoC.Proto2C()
-	id, err := pbftlinear.getClientID(ctx)
-	if err != nil {
-		logger.Printf("Failed to get client ID: %v", err)
-		return
-	}
-	dc.Sender = uint32(id)
-	pbftlinear.handleCommit(dc)
-}
-
-// 这个server是面向 集群内部的。
-type pbftLinearServer struct {
-	proto.UnimplementedPBFTLinearServer
-	*proto.GorumsServer
-	// maps a stream context to client info
-	mut     sync.RWMutex
-	clients map[context.Context]config.ReplicaID
-}
-
-func newPBFTLinearServer(pbftlinear *PBFTLinear, srv *proto.GorumsServer) *pbftLinearServer {
+func newPBFTLinearServer(pbftlinear *PBFTLinear) *pbftLinearServer {
 	pbftSrv := &pbftLinearServer{
-		PBFTLinear:   pbftlinear,
-		GorumsServer: srv,
-		clients:      make(map[context.Context]config.ReplicaID),
+		PBFTLinear: pbftlinear,
+		clients:    make(map[context.Context]config.ReplicaID),
 	}
 	return pbftSrv
 }
