bb3889c8449326ee442b2426395db28c8f73f380
zxh <178559145@qq.com>
2020-11-21 17:26:05 +0800

    pprof

--- cmd/pbftlinearserver/main.go
+++ cmd/pbftlinearserver/main.go
@@ -9,7 +9,6 @@ import (
 	"io/ioutil"
 	"log"
 	"net"
-	"net/http"
 	_ "net/http/pprof"
 	"os"
 	"os/signal"
@@ -171,12 +170,10 @@ func main() {
 
 	log.Printf("replica %d starts", conf.SelfID)
 
-	if conf.SelfID == 1 {
-		go func() {
-			err := http.ListenAndServe("127.0.0.1:6060", nil)
-			fmt.Printf("start http listen: %v\n", err)
-		}()
-	}
+	//go func() {
+	//	err := http.ListenAndServe(fmt.Sprintf("127.0.0.1:%d", 6060+conf.SelfID), nil)
+	//	fmt.Printf("start http listen: %v\n", err)
+	//}()
 
 	privkey, err := data.ReadPrivateKeyFile(conf.Privkey)
 	if err != nil {
@@ -256,7 +253,7 @@ func main() {
 		replicaConfig.Replicas[r.ID] = info
 	}
 	replicaConfig.ClusterSize = len(replicaConfig.Replicas)
-	replicaConfig.QuorumSize = 2*((len(replicaConfig.Replicas)-1)/3)+1 // pbft: 2f+1
+	replicaConfig.QuorumSize = 2*((len(replicaConfig.Replicas)-1)/3) + 1 // pbft: 2f+1
 
 	srv := newPBFTLinearServer(&conf, replicaConfig)
 	err = srv.Start(clientAddress)
